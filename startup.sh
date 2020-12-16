df189722f01a6fa73f88dd18f27329564f72bcca
ope079 <o_orekoya@yahoo.com>
2020-12-16 11:34:00 +0000

    changed status.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
-cd /opt/expenditure-tracker
-sudo mkdir /flask_project
-sudo chown -R /flask_project
-sudo python3 -m venv venv
+sudo apt update
+sudo apt-get install python3-venv
+
+
+# Test Phase
+python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
-python3 app.py
+
+
+# pytest goes here
+pytest --cov ./application
+
+# Deploy Phase
+
+
+# Make the installation directory
+sudo mkdir /opt/expenditure-tracker
+
+
+# Give jenkins user permissions for the installation directory
+sudo chown -R jenkins /opt/expenditure-tracker
+
+
+sudo systemctl daemon-reload
+sudo systemctl stop expenditure-tracker.service
+sudo systemctl start expenditure-tracker.service

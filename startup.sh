77d221d89ec845f559ed8747c4247891add5828e
ope079 <o_orekoya@yahoo.com>
2020-12-16 12:14:25 +0000

    edited startup.sh

--- startup.sh
+++ startup.sh
@@ -7,15 +7,15 @@ sudo apt-get install python3-venv
 python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
-python3 create.py
-python3 app.py
+
 
 
 # pytest goes here
-#pytest --cov ./application
+pytest --cov ./application
 
 # Deploy Phase
-
+python3 create.py
+python3 app.py
 
 # Make the installation directory
 sudo mkdir /opt/expenditure-tracker

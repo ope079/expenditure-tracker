49e468f76f009e622400424817de9cfd481a552b
ope079 <o_orekoya@yahoo.com>
2020-12-17 16:07:12 +0000

    edited int tests

--- tests/test_int.py
+++ tests/test_int.py
@@ -22,11 +22,14 @@ class TestBase(LiveServerTestCase):
 
     def setUp(self):
         print("--------------------------NEXT-TEST----------------------------------------------")
-        #chrome_options = Options()
+        chrome_options = Options()
         #chrome_options.binary_location = "/usr/bin/chromium-browser"
-        #chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
         self.driver = webdriver.Chrome(
-            #executable_path="/home/o_orekoya/chromedriver", chrome_options=chrome_options
+            #executable_path="/home/o_orekoya/chromedriver", 
+            chrome_options=chrome_options
             )
         self.driver.get("http://localhost:5000")
         

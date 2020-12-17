2319344566940b6daf8b859cc717507330c1d5e0
ope079 <o_orekoya@yahoo.com>
2020-12-17 15:54:10 +0000

    edited int test

--- tests/test_int.py
+++ tests/test_int.py
@@ -25,7 +25,9 @@ class TestBase(LiveServerTestCase):
         chrome_options = Options()
         chrome_options.binary_location = "/usr/bin/chromium-browser"
         chrome_options.add_argument("--headless")
-       # self.driver = webdriver.Chrome(executable_path="/home/o_orekoya/chromedriver", chrome_options=chrome_options)
+        self.driver = webdriver.Chrome(
+            #executable_path="/home/o_orekoya/chromedriver", chrome_options=chrome_options
+            )
         self.driver.get("http://localhost:5000")
         
         db.session.commit()

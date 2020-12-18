bc5763cdcb649b97fc842093f0bde7d24846e31d
ope079 <o_orekoya@yahoo.com>
2020-12-18 17:40:48 +0000

    updated startup.sh

--- startup.sh
+++ startup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-cd/opt/expenditure-tracker
+cd /opt/expenditure-tracker
 sudo mkdir /expenditure-tracker
 sudo chown -R /expenditure-tracker
 sudo python3 -m venv venv
 source venv/bin/activate
 pip3 install -r requirements.txt
-pyhton3 create.py
+python3 create.py
 python3 app.py
 
 
